0780b81aaa46f5d0867164e5585be2569a53e258
Sean Morrison <student@epicodus.com>
2014-09-10 14:24:53 -0700

    Generate a stripe token on form submit.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -4,6 +4,37 @@ class DonationsController < ApplicationController
   end
 
   def new
+
+  end
+
+  def create
+    # Set your secret key: remember to change this to your live secret key in production
+    # See your keys here https://dashboard.stripe.com/account
+    Stripe.api_key = "sk_test_BQokikJOvBiI2HlWgH4olfQ2"
+
+    # Get the credit card details submitted by the form
+    token = params[:stripeToken]
+
+    # Create the charge on Stripe's servers - this will charge the user's card
+    begin
+      charge = Stripe::Charge.create(
+        :amount => 1000, # amount in cents, again
+        :currency => "usd",
+        :card => token,
+        :description => "payinguser@example.com"
+      )
+      flash[:notice] = "Payment accepted."
+      redirect_to users_path
+    rescue Stripe::CardError => e
+      # The card has been declined
+      flash[:alert] = "Payment declined."
+      render 'new'
+    end
+  end
+
+  private
+  def donation_params
+    params.require(:donation).permit(:stripeToken,:amount, :currency, :card, :description)
   end
 
 end
